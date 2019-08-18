8fdc7339dbf3df0ed460730e6b4422ce1abbef14
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-18 21:41:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ class User {
         return event.keywords.some(
             function(word) {
                 return this.interests.includes(word);
-            }.bind(this) // added to the and of the callback function
+            }.bind(this) 
         );
     }
 }
